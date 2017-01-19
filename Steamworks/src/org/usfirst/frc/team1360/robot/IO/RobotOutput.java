287f6c5e1d9a1789ad0445644693966ff1bb4263
Palvizar <kevinz999@gmail.com>
2017-01-18 20:16:38 -0500

    Finished AutonDrive and Tankdrive

--- Steamworks/src/org/usfirst/frc/team1360/robot/IO/RobotOutput.java
+++ Steamworks/src/org/usfirst/frc/team1360/robot/IO/RobotOutput.java
@@ -1,12 +1,22 @@
 package org.usfirst.frc.team1360.robot.IO;
 
+import edu.wpi.first.wpilibj.Victor;
+
 public class RobotOutput {
+	
+	private Victor driveLeftForward;
+	private Victor driveLeftBackward;
+	private Victor driveRightForward;
+	private Victor driveRightBackward;
 	
 	private static  RobotOutput instance;
 	
 	private RobotOutput()
 	{
-		
+		driveLeftForward = new Victor(0);
+		driveLeftBackward = new Victor(1);
+		driveRightForward = new Victor(2);
+		driveRightBackward = new Victor(3);
 	}
 	
 	public static RobotOutput getInstance()
@@ -19,6 +29,13 @@ public class RobotOutput {
 		return instance;
 	}
 	
+	public void tankDrive(double left, double right)
+	{
+		driveLeftForward.set(-left);
+		driveLeftBackward.set(-left);
+		driveRightForward.set(right);
+		driveRightBackward.set(right);
+}
 	public void stopAll()
 	{
 		
