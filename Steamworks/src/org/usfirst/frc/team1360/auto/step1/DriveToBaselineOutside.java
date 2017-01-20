d4f1320d0c57579d34bb5e7748ea585e557a1128
Cathal Kenneally <littlemanck@gmail.com>
2017-01-19 19:56:21 -0500

    DriveToBaselineOutside should now work
    DriveToBaselineOutisde drives forward 3 sec, then waits 12 sec.

--- Steamworks/src/org/usfirst/frc/team1360/auto/step1/DriveToBaselineOutside.java
+++ Steamworks/src/org/usfirst/frc/team1360/auto/step1/DriveToBaselineOutside.java
@@ -1,5 +1,19 @@
 package org.usfirst.frc.team1360.auto.step1;
 
-public class DriveToBaselineOutside {
+import org.usfirst.frc.team1360.auto.drive.AutonDrive;
+import org.usfirst.frc.team1360.auto.drive.DriveWait;
+import org.usfirst.frc.team1360.auto.mode.AutonBuilder;
+import org.usfirst.frc.team1360.auto.mode.AutonMode;
+import org.usfirst.frc.team1360.auto.util.AutonWait;
+
+public class DriveToBaselineOutside implements AutonMode  {
+
+	@Override
+	public void addToMode(AutonBuilder ab) {
+		// TODO Auto-generated method stub
+		ab.addCommand(new AutonDrive(3000,0.75, 0.75));
+		ab.addCommand(new DriveWait());
+		ab.addCommand(new AutonWait(12000));
+	}
 
 }
