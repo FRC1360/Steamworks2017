64d95e0f7b241fc3fbdf00164c2e87be0c15267b
Nicholas Mertin <nmertin@mertin.home>
2017-01-23 11:44:34 -0500

    Added connection to robotInit

--- Steamworks/src/org/usfirst/frc/team1360/robot/Robot.java
+++ Steamworks/src/org/usfirst/frc/team1360/robot/Robot.java
@@ -1,10 +1,13 @@
 
 package org.usfirst.frc.team1360.robot;
 
+import java.io.IOException;
+
 import org.usfirst.frc.team1360.robot.IO.HumanInput;
 import org.usfirst.frc.team1360.robot.IO.RobotOutput;
 import org.usfirst.frc.team1360.robot.IO.SensorInput;
 import org.usfirst.frc.team1360.robot.teleop.TeleopControl;
+import org.usfirst.frc.team1360.server.Connection;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Timer;
@@ -17,6 +20,7 @@ public class Robot extends IterativeRobot {
 	private HumanInput humanInput;
 	private SensorInput sensorInput;
 	private TeleopControl teleopControl;
+	private Connection connection;
 	
 	
     public void robotInit() 
@@ -25,8 +29,21 @@ public class Robot extends IterativeRobot {
     	this.humanInput = HumanInput.getInstance();
     	this.teleopControl = TeleopControl.getInstance();
     	this.sensorInput = SensorInput.getInstance();
+    	try
+    	{
+			this.connection = new Connection(5800);
+		}
+    	catch (IOException e)
+    	{
+			System.err.println("Unable to open connection to driver station!");
+			e.printStackTrace();
+		}
     }
     
+    public Connection GetConnection()
+    {
+    	return connection;
+    }
 
     public void autonomousInit() 
     {
