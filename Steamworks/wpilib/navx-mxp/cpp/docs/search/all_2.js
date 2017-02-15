388ed0ebaadb585f47784e2b9d812b945da348cc
MichGedd <michaelgeddes555@gmail.com>
2017-02-15 13:21:53 -0500

    After lots of trial and error, I finaally added the NavX lib

---
+++ Steamworks/wpilib/navx-mxp/cpp/docs/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['deregistercallback',['DeregisterCallback',['../class_a_h_r_s.html#a597cfd9ca39b54db14e07ed818d1b368',1,'AHRS']]]
+];
---
+++ Steamworks/wpilib/navx-mxp/cpp/docs/search/functions_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['setangleadjustment',['SetAngleAdjustment',['../class_a_h_r_s.html#a17af5541b6699fae5d5c0264438d52ae',1,'AHRS']]]
+];
