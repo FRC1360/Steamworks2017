2383b1ede340887a5e39198f187ec0522c4636f1
Jason H <heddlejason@gmail.com>
2017-02-19 11:10:33 -0500

    Added getPosition function

---
+++ Steamworks/src/org/usfirst/frc/team1360/auto/step1/NavxTest.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team1360.auto.step1;
+
+import org.usfirst.frc.team1360.auto.mode.AutonBuilder;
+import org.usfirst.frc.team1360.auto.mode.AutonMode;
+
+public class NavxTest implements AutonMode {
+
+	@Override
+	public void addToMode(AutonBuilder ab) {
+		
+	}
+
+}
