92cbfa6ccb3396f4fd67ab6cd45589bfd181ea3e
Nicholas Mertin <nmertin@mertin.home>
2017-02-27 18:28:10 -0500

    Worked on position tracking

--- Steamworks/src/org/usfirst/frc/team1360/navx/PositionTracker.java
+++ Steamworks/src/org/usfirst/frc/team1360/navx/PositionTracker.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.I2C;
 
 public class PositionTracker {
 	private static PositionTracker instance;
-	AHRS ahrs;
 	RobotOutput robotOutput;
 	SensorInput sensorInput;
 	double[] position; // First item in array is x value second item in array is y value in CENTIMETRES
@@ -17,8 +16,8 @@ public class PositionTracker {
 	long prevTime;
 	long thisTime;
 	long timeDiff;
-	float thisYaw;
-	float prevYaw;
+	double thisYaw;
+	double prevYaw;
 	double theta;
 	
 	public static PositionTracker getInstance() {
@@ -28,24 +27,19 @@ public class PositionTracker {
 		return instance;
 	}
 	
-	public AHRS getAHRS() {
-		return ahrs;
-	}
-	
 	// this constructor must be called in the first few lines of code or else it will not work properly
 	private PositionTracker() {
 		sensorInput = SensorInput.getInstance();
 		robotOutput = RobotOutput.getInstance();
-		ahrs = SensorInput.getInstance().ahrs;
 		position = new double[] { 0.0, 0.0 };
 		velocity = new double[] { 0.0, 0.0 };
 		prevTime = System.nanoTime();
-		prevYaw = ahrs.getYaw();
+		prevYaw = sensorInput.getAHRSYaw();
 		
 		(new Thread() {
 			public void run() {
 				while (true) {
-					thisYaw = ahrs.getYaw();
+					thisYaw = sensorInput.getAHRSYaw();
 					if (thisYaw > prevYaw)
 						while (thisYaw - prevYaw >= 360.0)
 							thisYaw -= 360.0;
@@ -57,17 +51,17 @@ public class PositionTracker {
 					timeDiff = (thisTime - prevTime);
 					prevTime = thisTime;
 					
-					double x = ahrs.getWorldLinearAccelX();
-					double y = ahrs.getWorldLinearAccelY();
+					double x = sensorInput.getAHRSWorldLinearAccelX();
+					double y = sensorInput.getAHRSWorldLinearAccelY();
 					velocity[0] += x * timeDiff;
 					velocity[1] += y * timeDiff;
 					position[0] += velocity[0] * timeDiff;
 					position[1] += velocity[1] * timeDiff;
 					
-					/*System.out.printf("%f\n%f\n", velocity[0], velocity[1]);
-					System.out.printf("%f\n%f\n", x * timeDiff, y * timeDiff);
-					System.out.printf("%f\n%f\n", SensorInput.getInstance().ahrs.getVelocityX(), SensorInput.getInstance().ahrs.getVelocityY());
-					System.out.println(timeDiff + "\n\n\n");*/
+					//System.out.printf("%f\n%f\n", velocity[0], velocity[1]);
+					//System.out.printf("%f\n%f\n", x * timeDiff, y * timeDiff);
+					//System.out.printf("%f\n%f\n", SensorInput.getInstance().ahrs.getVelocityX(), SensorInput.getInstance().ahrs.getVelocityY());
+					//System.out.println(timeDiff + "\n\n\n");
 					
 					Thread.yield();
 				}
