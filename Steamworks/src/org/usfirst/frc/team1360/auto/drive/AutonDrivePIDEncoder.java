672e1c278adde56d405c9eed4ffcebd97f54e6df
Nicholas Mertin <nmertin@mertin.home>
2017-03-09 11:33:07 -0500

    Merged after competition

--- Steamworks/src/org/usfirst/frc/team1360/auto/drive/AutonDrivePIDEncoder.java
+++ Steamworks/src/org/usfirst/frc/team1360/auto/drive/AutonDrivePIDEncoder.java
@@ -7,7 +7,8 @@ import org.usfirst.frc.team1360.robot.IO.SensorInput;
 import org.usfirst.frc.team1360.robot.util.OrbitPID;
 
 public class AutonDrivePIDEncoder extends AutonCommand {
-
+	private static double SCALAR = 3.70 / 3.94;
+	//private static double SCALAR = 0.75;
 
 	private SensorInput sensorInput;
 	private RobotOutput robotOutput;
@@ -25,7 +26,7 @@ public class AutonDrivePIDEncoder extends AutonCommand {
 	public AutonDrivePIDEncoder(double target, double speed, int encoderLimit, long timeout)
 	{
 		this(target, speed, 0.5, timeout);
-        this.encoderLimit = encoderLimit;
+        this.encoderLimit = (int)(encoderLimit * SCALAR);
 	}
 	
 	public AutonDrivePIDEncoder(double target, double speed, double eps, long timeout) {
@@ -74,6 +75,7 @@ public class AutonDrivePIDEncoder extends AutonCommand {
         }
 
         int diff = sensorInput.getLeftDriveEncoder() - encoderStart;
+
         return encoderLimit > 0 ? diff > encoderLimit : encoderLimit > diff;
 	}
 
---
+++ Steamworks/src/org/usfirst/frc/team1360/auto/step2/NeutralZoneAfterSideGear.java
@@ -0,0 +1,25 @@
+package org.usfirst.frc.team1360.auto.step2;
+
+import org.usfirst.frc.team1360.auto.drive.AutonDrivePIDEncoder;
+import org.usfirst.frc.team1360.auto.drive.AutonDrivePIDTime;
+import org.usfirst.frc.team1360.auto.drive.AutonDrivePIDTurn;
+import org.usfirst.frc.team1360.auto.drive.AutonDriveShift;
+import org.usfirst.frc.team1360.auto.drive.DriveWait;
+import org.usfirst.frc.team1360.auto.gear.AutonGear;
+import org.usfirst.frc.team1360.auto.mode.AutonBuilder;
+import org.usfirst.frc.team1360.auto.mode.AutonMode;
+import org.usfirst.frc.team1360.auto.util.AutonWait;
+
+public class NeutralZoneAfterSideGear implements AutonMode
+{
+
+	@Override
+	public void addToMode(AutonBuilder ab)
+	{
+		// Go to middle of field
+		ab.addCommand(new AutonDriveShift(true));
+		ab.addCommand(new AutonDrivePIDEncoder(0, 1, 4000, 10000));
+        ab.addCommand(new AutonDriveShift(false));
+	}
+
+}
