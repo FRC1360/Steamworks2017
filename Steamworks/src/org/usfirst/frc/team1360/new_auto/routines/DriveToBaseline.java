7eb399aa054885b631de9119218cd9e082d37475
Michael Geddes <michaelgeddes555@gmail.com>
2017-10-28 22:30:04 -0400

    Changed drive to baseline auto to use PID/Enc

--- Steamworks/src/org/usfirst/frc/team1360/new_auto/routines/DriveToBaseline.java
+++ Steamworks/src/org/usfirst/frc/team1360/new_auto/routines/DriveToBaseline.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1360.new_auto.routines;
 
 import org.usfirst.frc.team1360.new_auto.AutonRoutine;
+import org.usfirst.frc.team1360.new_auto.drive.DrivePIDEncoder;
 
 public class DriveToBaseline extends AutonRoutine {
 	public DriveToBaseline() {
@@ -10,8 +11,6 @@ public class DriveToBaseline extends AutonRoutine {
 	@Override
 	public void runCore() throws InterruptedException
 	{
-		robotOutput.tankDrive(1.0, 1.0);
-		Thread.sleep(1000);
-		robotOutput.stopAll();
+		new DrivePIDEncoder(0.0, 1.0, 800, 4000);
 	}
 }
