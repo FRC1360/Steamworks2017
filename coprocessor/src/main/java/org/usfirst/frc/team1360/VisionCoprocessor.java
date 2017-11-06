9d2d3bf8a092058680bf6ba49e55b7bb52a19559
Constantine Yevenko <cyevenko@outsideiq.com>
2017-11-05 22:44:44 -0500

    Upgraded Gradle and GradleRIO. Moved sources into src/main/java. Added coprocessor module.

---
+++ coprocessor/src/main/java/org/usfirst/frc/team1360/VisionCoprocessor.java
@@ -0,0 +1,7 @@
+package org.usfirst.frc.team1360;
+
+public class VisionCoprocessor {
+    public static void main(String[] args) {
+        System.out.println("I can see what you see not");
+    }
+}
