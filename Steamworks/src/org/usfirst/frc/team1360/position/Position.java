987b780af67d65c399e98c54aaa308aa67d163c0
Nick Mertin <nickmertin@gmail.com>
2017-11-27 21:34:31 -0500

    Finished support classes for position tracking

--- Steamworks/src/org/usfirst/frc/team1360/position/Position.java
+++ Steamworks/src/org/usfirst/frc/team1360/position/Position.java
@@ -1,14 +1,32 @@
 package org.usfirst.frc.team1360.position;
 
 public final class Position implements Cloneable {
-	private double x; // X-coordinate (right from center of driver station = positive)
-	private double y; // Y-coordinate (forward from driver station wall = positive)
-	private double a; // Angle (clockwise from forward = positive)
+	private double x; // X-coordinate (inches, right from center of driver station = positive)
+	private double y; // Y-coordinate (inches, forward from driver station wall = positive)
+	private double a; // Angle (radians, clockwise from forward = positive)
 	
 	public Position(double x, double y, double a) {
 		this.x = x;
 		this.y = y;
 		this.a = a;
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public double getA() {
+		return a;
+	}
+	
+	public void offset(double x, double y, double z) {
+		this.x += x;
+		this.y += y;
+		this.a += a;
 	}
 	
 	@Override
--- Steamworks/src/org/usfirst/frc/team1360/position/PositionProvider.java
+++ Steamworks/src/org/usfirst/frc/team1360/position/PositionProvider.java
@@ -1,5 +1,19 @@
 package org.usfirst.frc.team1360.position;
 
 public interface PositionProvider {
+	double getX();
+	double getY();
+	double getA();
 	
+	void reset(double x, double y, double z);
+	
+	default void reset() {
+		reset(0, 0, 0);
+	}
+	
+	default Position getPosition() {
+		synchronized (this) {
+			return new Position(getX(), getY(), getA());
+		}
+	}
 }
